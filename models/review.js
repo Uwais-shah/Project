ef16e0ada1c3b8ab2823653fa38d58db4a0cb258
Uwais-shah <142101435+Uwais-shah@users.noreply.github.com>
2024-08-06 14:10:05 +0530

    Add files via upload

---
+++ models/review.js
@@ -0,0 +1,16 @@
+const { string, array } = require("joi");
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const reviewSchema = new Schema({
+    comment : String,
+    rating : Number,
+
+    createdAt : {
+        type : Date,
+        default : Date.now()
+    },
+
+});
+
+module.exports = mongoose.model("Review", reviewSchema);
